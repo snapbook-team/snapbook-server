c97e08278a6ed0cba3a5191fb2608d50deb1a258
PleBea <bini060315kim@gmail.com>
2024-09-20 08:59:42 +0900

    feat(project): Add project module, controller, and service

---
+++ src/modules/project/project.controller.ts
@@ -0,0 +1,8 @@
+import { Controller } from "@nestjs/common";
+
+import { ProjectService } from "./project.service";
+
+@Controller("project")
+export class ProjectController {
+  constructor(private readonly projectService: ProjectService) {}
+}
